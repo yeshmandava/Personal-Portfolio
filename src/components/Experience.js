e7577c2cde834608981ee964267e085afe49ebf8
Yeshwanth Mandava <yeshwanthmandava10@gmail.com>
2023-08-16 01:38:56 -0400

    Create repo

---
+++ src/components/Experience.js
@@ -0,0 +1,110 @@
+import React, { useRef } from "react";
+import {
+  motion,
+  useScroll,
+} from "framer-motion";
+import LiIcon from "./LiIcon";
+
+
+const Details = ({ position, company, companyLink, time, address, work }) => {
+  const ref = useRef(null);
+  return (
+    <li
+      ref={ref}
+      className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-start justify-between md:w-[80%]"
+    >
+      <LiIcon reference={ref} />
+      <motion.div
+        initial={{ y: 50 }}
+        whileInView={{ y: 0 }}
+        transition={{ duration: 0.5, type: "spring" }}
+      >
+        <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
+          {position}{" "}
+          <a
+            className="capitalize text-primary dark:text-primaryDark"
+            href={companyLink}
+            target={"_blank"}
+          >
+            @{company}
+          </a>
+        </h3>
+        <span className="capitalize text-dark/75 font-medium dark:text-light/50 xs:text-sm">
+          {time} | {address}
+        </span>
+        <p className="font-medium w-full md:text-sm"> {work}</p>
+      </motion.div>
+    </li>
+  );
+};
+
+const Experience = () => {
+
+  const ref = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "center start"],
+  });
+
+  return (
+
+      <div className="my-64">
+        <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
+          Experience
+        </h2>
+
+        <div ref={ref} className="relative w-[75%] mx-auto lg:w-[90%] md:w-full">
+          <motion.div
+            className="absolute left-9 top-0 w-[4px] md:w-[2px] md:left-[30px] xs:left-[20px] h-full bg-dark 
+            origin-top  dark:bg-primaryDark dark:shadow-3xl"
+            style={{ scaleY: scrollYProgress }}
+          />
+          <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
+            <Details
+              position="Software Engineer Intern"
+              company=" Deloitte"
+              companyLink="https://www.deloitte.com/global/en/services/consulting.html"
+              time="June 2023 - August 2023"
+              address="Lake Mary, FL"
+              work="Worked on a team responsible for developing new features for Google's search engine, including improving the accuracy and relevance of search results and developing new tools for data analysis and visualization."
+            />
+
+            <Details
+              position="Quality Engineer Co-op"
+              company=" Lockheed Martin"
+              companyLink="https://www.lockheedmartin.com/"
+              time="August 2022 - Present"
+              address="Orlando, FL."
+              work="Worked on a team responsible for developing a new mobile app
+              feature that allowed users to create and share short-form video
+              content, including designing and implementing a new user interface
+              and developing the backend infrastructure to support the feature."
+            />
+
+            <Details
+              position="Software Developer Accelerate"
+              company=" IBM"
+              companyLink="https://www.ibm.com/us-en"
+              time="May 2022 - August 2022"
+              address="Remote"
+              work="Worked on a team responsible for developing Amazon's mobile app, including implementing new features such as product recommendations and user reviews, and optimizing the app's performance and reliability."
+            />
+
+            <Details
+              position="Software Engineering Experience"
+              company=" J.P. Morgan"
+              companyLink="https://www.jpmorgan.com/global"
+              time="January 2023 - February 2023"
+              address="Remote"
+              work="Engaged in the development of a stock price data analysis system using Python. 
+                    Utilized JPMorgan's Open Source Software, Perspective, to provide real-time actionable 
+                    metrics to a trader's dashboard with the help of React and Typescript."
+            />
+          </ul>
+        </div>
+        </div>
+    );
+};
+
+export default Experience;
