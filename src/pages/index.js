83a2f07f9bfc9c980afd7b6a9d4d17bd510fe8e8
Yeshwanth Mandava <yeshwanthmandava10@gmail.com>
2023-08-17 18:40:10 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,11 +50,11 @@ export default function Home() {
           {/* <div className="flex w-full justify-center"> */}
           <div className="flex flex-col items-center md:items-start sm:items-start"> {/* Center on mobile, align start on medium and larger screens */}
 
-            <div className=" w-1/2 md:w-1/1 sm:w-1/1 xl:1/1 flex flex-col items-start self-center">
+            <div className=" w-1/1 md:w-1/1 sm:w-1/1 xl:1/1 flex flex-col items-start self-center">
 
-              <div className="text-left w-1/1"> 
+              {/* <div className="text-left ">  */}
                 <AnimatedText text="Hey, I'm __" className=" text-left font-normal pr-0 sm:pr-0 md:pr-0" />
-               </div>
+               {/* </div> */}
 
               <div className="font-bold text-blue-500 text-4xl">
                 <Typewriter
