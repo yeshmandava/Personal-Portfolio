dad61c1fcb49aaf075095becdd73708714dd9105
Yeshwanth Mandava <yeshwanthmandava10@gmail.com>
2023-08-17 16:20:07 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -53,7 +53,7 @@ export default function Home() {
             <div className="w-1/2 md:w-1/1 sm:w-1/1 flex flex-col items-start self-center">
 
               {/* <div className="text-left text-3xl"> */}
-                <AnimatedText text="Hey, I'm __" className=" text-left font-normal pr-1 sm:pr-1 md:pr-1" />
+                <AnimatedText text="Hey, I'm __" className=" text-left font-normal pr-0 sm:pr-0 md:pr-0" />
               {/* </div> */}
 
               <div className="font-bold text-blue-500 text-4xl">
